3cd394131565202690dbe72a0c6f8b1f8b4d7a4d
akux2021 <32490390+chao0716@users.noreply.github.com>
2022-08-31 22:52:43 +0800

    Create collect_data.py

---
+++ collect_data.py
@@ -0,0 +1,7 @@
+from generate_roll_grasp import generate_grasps as roll
+from generate_side_grasp import generate_grasps as side
+from generate_td_grasp import generate_grasps as td
+
+roll(10000)
+side(10000)
+td(10000)
